4987d2988d6f088fc497f31d6119a703cff3d4cc
nahidnsu152 <nahid.blackeyes@gmail.com>
2024-06-04 18:01:12 +0600

    Code refactored

--- lib/dio_manager/src/dio_factory.dart
+++ lib/dio_manager/src/dio_factory.dart
@@ -1,3 +1,50 @@
+// // import 'package:dio/dio.dart';
+// // import 'package:flutter/foundation.dart';
+// // import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+// // class DioFactory {
+// //   late String _token = '';
+// //   late String _baseUrl;
+
+// //   void setToken(String token) {
+// //     _token = token;
+// //   }
+
+// //   void setBaseUrl(String baseUrl) {
+// //     _baseUrl = baseUrl;
+// //   }
+
+// //   Future<Dio> getDio() async {
+// //     Dio dio = Dio();
+
+// //     Map<String, String> headers = {
+// //       "content-type": "application/json",
+// //       "accept": "application/json",
+// //       if (_token.isNotEmpty) "authorization": "Bearer $_token",
+// //       "language": "en"
+// //     };
+
+// //     dio.options = BaseOptions(
+// //       baseUrl: _baseUrl,
+// //       headers: headers,
+// //       receiveTimeout: const Duration(milliseconds: 30000),
+// //       sendTimeout: const Duration(milliseconds: 30000),
+// //     );
+
+// //     if (!kReleaseMode) {
+// //       dio.interceptors.add(PrettyDioLogger(
+// //         requestHeader: true,
+// //         requestBody: true,
+// //         responseHeader: true,
+// //       ));
+// //     }
+
+// //     return dio;
+// //   }
+// // }
+
+// import 'dart:async';
+
 // import 'package:dio/dio.dart';
 // import 'package:flutter/foundation.dart';
 // import 'package:pretty_dio_logger/pretty_dio_logger.dart';
@@ -5,6 +52,7 @@
 // class DioFactory {
 //   late String _token = '';
 //   late String _baseUrl;
+//   Map<String, String>? _additionalHeaders;
 
 //   void setToken(String token) {
 //     _token = token;
@@ -14,6 +62,10 @@
 //     _baseUrl = baseUrl;
 //   }
 
+//   void setAdditionalHeaders(Map<String, String>? headers) {
+//     _additionalHeaders = headers;
+//   }
+
 //   Future<Dio> getDio() async {
 //     Dio dio = Dio();
 
@@ -21,7 +73,8 @@
 //       "content-type": "application/json",
 //       "accept": "application/json",
 //       if (_token.isNotEmpty) "authorization": "Bearer $_token",
-//       "language": "en"
+//       "language": "en",
+//       if (_additionalHeaders != null) ..._additionalHeaders!,
 //     };
 
 //     dio.options = BaseOptions(
@@ -42,56 +95,3 @@
 //     return dio;
 //   }
 // }
-
-import 'dart:async';
-
-import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
-import 'package:pretty_dio_logger/pretty_dio_logger.dart';
-
-class DioFactory {
-  late String _token = '';
-  late String _baseUrl;
-  Map<String, String>? _additionalHeaders;
-
-  void setToken(String token) {
-    _token = token;
-  }
-
-  void setBaseUrl(String baseUrl) {
-    _baseUrl = baseUrl;
-  }
-
-  void setAdditionalHeaders(Map<String, String>? headers) {
-    _additionalHeaders = headers;
-  }
-
-  Future<Dio> getDio() async {
-    Dio dio = Dio();
-
-    Map<String, String> headers = {
-      "content-type": "application/json",
-      "accept": "application/json",
-      if (_token.isNotEmpty) "authorization": "Bearer $_token",
-      "language": "en",
-      if (_additionalHeaders != null) ..._additionalHeaders!,
-    };
-
-    dio.options = BaseOptions(
-      baseUrl: _baseUrl,
-      headers: headers,
-      receiveTimeout: const Duration(milliseconds: 30000),
-      sendTimeout: const Duration(milliseconds: 30000),
-    );
-
-    if (!kReleaseMode) {
-      dio.interceptors.add(PrettyDioLogger(
-        requestHeader: true,
-        requestBody: true,
-        responseHeader: true,
-      ));
-    }
-
-    return dio;
-  }
-}
